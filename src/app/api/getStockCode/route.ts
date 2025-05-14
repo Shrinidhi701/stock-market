a7572b88a2a35d87814f903f6aed59ecfaab2054
Shrinidhi701 <83268420+Shrinidhi701@users.noreply.github.com>
2025-05-14 09:24:11 +0530

    removed console.log() and extra spaces

--- src/app/api/getStockCode/route.ts
+++ src/app/api/getStockCode/route.ts
@@ -5,12 +5,9 @@ import { parse } from 'csv-parse/sync';
 export const runtime = 'nodejs';
 
 export async function GET(req: Request) {
-  console.log("API route hit.");
-
   const stockName = req.headers.get('stockName') || "";
   const results: Array<Object> = [];
   const fileUrl = 'https://nsearchives.nseindia.com/content/equities/namechange.csv';
-
   try {
     const response = await axios.get(fileUrl, {
       responseType: 'arraybuffer',
@@ -21,21 +18,15 @@ export async function GET(req: Request) {
         'Origin': 'https://www.nseindia.com'
       }
     });
-
     const csvText = Buffer.from(response.data, 'binary').toString('utf-8');
-
-    // Parse CSV into an array of objects
     const records = parse(csvText, {
-      columns: true,           // Use first row as header
+      columns: true,
       skip_empty_lines: true,
       trim: true
     });
-
-    // Filter based on stockName
     for (const row of records) {
       const prev = row['NCH_PREV_NAME']?.toLowerCase() || '';
       const current = row['NCH_NEW_NAME']?.toLowerCase() || '';
-
       if (
         prev.includes(stockName.toLowerCase()) ||
         current.includes(stockName.toLowerCase())
@@ -48,13 +39,10 @@ export async function GET(req: Request) {
         });
       }
     }
-
     if (results.length === 0) {
       return NextResponse.json({ message: 'No matching stock found' }, { status: 404 });
     }
-
     return NextResponse.json(results, { status: 200 });
-
   } catch (error: any) {
     console.error("Error parsing CSV:", error.message);
     return NextResponse.json(
